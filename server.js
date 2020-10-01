fccfc82b76f0359d15bb40a91b3d325b405b8c61
Patel Jeet <you@example.com>
2020-09-30 20:04:49 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,22 +4,8 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-]
-};
+// Trying to read from the budget-data.json file
+const budget = require ("./budget");
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
