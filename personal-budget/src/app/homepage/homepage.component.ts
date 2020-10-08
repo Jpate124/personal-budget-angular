3f6ddfebea717811413e5b85a9f06e6df47bf3e0
Patel Jeet <you@example.com>
2020-10-08 15:41:46 -0400

    Added Angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+public dataSource = {
+  datasets: [
+      {
+          data: [],
+          backgroundColor: [
+              '#8E44AD',
+              '#2C3E50',
+              '#DAF7A6',
+              '##FFC300',
+              '#FF5733',
+              '#C70039',
+              '#900C3F',
+              '#581845'
+          ],
+      }
+  ],
+  labels: []
+};
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/budget')
+    .subscribe((res: any) => {
+      for (var i = 0; i < res.myBudget.length; i++) {
+        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+        this.dataSource.labels[i] = res.myBudget[i].title;
+        this.createChart();
+      }
+    });
+  }
+  createChart() {
+    // var ctx = document.getElementById("myChart").getContext("2d");
+    var ctx = document.getElementById('myChart');
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: this.dataSource
+    });
+}
+
+
+}
